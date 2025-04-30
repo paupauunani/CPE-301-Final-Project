30b2819c39adfc54c903860a05274a6965f77df1
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-04-29 18:26:50 -0700

    define FOSC and BAUD. write USART_init

--- driver.c
+++ driver.c
@@ -1,7 +1,16 @@
 // Written by Alysia Carr, Paulane Tulop, Shira Rotem, William Chuter-Davies
 
+#define FOSC 16000000
+#define BAUD 9600
+
+void USART_init(unsigned char ubrr)
+{       UBRRH = (unsigned char)ubrr >> 8;
+        UBBRL = (unsigned char)ubrr;
+        UCSRB = (1 << RXEN) | (1 << TXEN);
+        UCSRC = (1 << USBS) | (3 << UCSZ0);
+}
 void setup(void)
-{       
+{       USART_init((FOSC / (16 * BAUD)) - 1);
 }
 void loop(void)
 {
