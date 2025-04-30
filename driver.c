c070f02b9fb17ab71874511440ab37d55b5b61f6
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-04-29 18:49:16 -0700

    tweak header comments. remove default register declarations.

--- driver.c
+++ driver.c
@@ -1,41 +1,10 @@
-//Written by Alysia Carr, Paulane Tulop, Shira Rotem, William Chuter-Davies
-
-//What libraries can be used
-//If using on VSC, may need to configure file path for library; Already in Arduino IDE
-//#include <LiquidCrystal.h> - had to comment out
-
-//Register values for pins need to decide what pins we are using for input/output
-
-//Register values for Timer Theory
-volatile unsigned char *myTCCR1A = (unsigned char *) 0x80;
-volatile unsigned char *myTCCR1B = (unsigned char *) 0x81;
-volatile unsigned char *myTCCR1C = (unsigned char *) 0x82;
-volatile unsigned char *myTIMSK1 = (unsigned char *) 0x6F;
-volatile unsigned int  *myTCNT1  = (unsigned  int *) 0x84;
-volatile unsigned char *myTIFR1 =  (unsigned char *) 0x36;
-
-//Register values for USART - used in output on Serial Monitor
- volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
- volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
- volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
- volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
- volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
-
-//Register values for ADC (analog to digital conversion)
-volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
-volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
-volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
-volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
-
+// written by Alysia Carr, William Chuter-Davies, Shira Rotem, Paulane Tulop
 
 #define FOSC 16000000
 #define BAUD 9600
 
 void USART_init(unsigned char ubrr)
-{       UBRRH = (unsigned char)ubrr >> 8;
-        UBBRL = (unsigned char)ubrr;
-        UCSRB = (1 << RXEN) | (1 << TXEN);
-        UCSRC = (1 << USBS) | (3 << UCSZ0);
+{       
 }
 void setup(void)
 {       USART_init((FOSC / (16 * BAUD)) - 1);
