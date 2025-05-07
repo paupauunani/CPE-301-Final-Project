bd75da704924aa419532964ba282aea7dfecf7a4
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-05-07 12:47:26 -0700

    fix type errors. tweak comments.  tweak formatting. clean up rx and tx functions
    7 - 9: fix type error: int* to char*
    14: fix id erro: myUDRO to myUDR0
    60 - 65: write usart_rx to replace u0_getchar and u0_kbhit
    67 - 72: write usart_tx to replace u0_getchar

--- driver.c
+++ driver.c
@@ -4,29 +4,24 @@
 // #include <LiquidCrystal.h>
 
 /* registers required for adc functionality */
-volatile unsigned int* myADMUX = (unsigned char*) 0x7C;
-volatile unsigned int* myADCSRB = (unsigned char*) 0x7B;
-volatile unsigned int* myADCSRA = (unsigned char*) 0x7A;
+volatile unsigned char* myADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* myADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* myADCSRA = (unsigned char*) 0x7A;
 volatile unsigned char* myADCH = (unsigned char*) 0x79;
 volatile unsigned char* myADCL = (unsigned char*) 0x78;
 
 /* registers required for usart functionality */
-volatile unsigned char* myUDRO = (unsigned char*) 0xC6;
+volatile unsigned char* myUDR0 = (unsigned char*) 0xC6;
 volatile unsigned char* myUBRR0H = (unsigned char*) 0xC5;
 volatile unsigned char* myUBRR0L = (unsigned char*) 0xC4;
 volatile unsigned char* myUCSR0C = (unsigned char*) 0xC2;
 volatile unsigned char* myUCSR0B = (unsigned char*) 0xC1;
-volatile unsigned char* myUSCR0A = (unsigned char*) 0xC0;
-
-#define fosc 16000000 
-#define baud 9600
-#define RDA 0x80
-#define TBE 0x20
+volatile unsigned char* myUCSR0A = (unsigned char*) 0xC0;
 
 void adc_init()
 {       /* clear adc multiple selection register */
         *myADMUX = 0b00000000;
-        /* set voltage reference */
+        /* set voltage reference to AVcc */
         *myADMUX |= 0b01000000;
         /* clear adc control and status register b */
         *myADCSRB = 0b00000000;
@@ -34,63 +29,55 @@ void adc_init()
         *myADCSRA = 0b00000000;
         /* enable adc */
         *myADCSRA |= 0b10000000;
-        /* set pre-scaler */
+        /* set pre-scaler to 128 */
         *myADCSRA |= 0b00000111;
 } /* adc_init */
-unsigned int adc_read(unsigned char adc_channel)
-{       /* clear input channel */
+
+unsigned int adc_read(unsigned char adc_input_channel)
+{       /* clear input selection */
         *myADMUX &= 0b11100000;
-        /* set input channel */
-        *myADMUX |= adc_channel & 0b00000111;
-        /* start adc conversion */
+        /* set input selection to adc_input_channel */
+        *myADMUX |= adc_input_channel & 0b00000111;
+        /* start analog-digital conversion */
         *myADCSRA |= 0b01000000;
-        /* wait until conversion is completed */
+        /* wait until conversion is complete */
         while(*myADCSRA & 0b01000000);
         /* return conversion result */
         return *myADCL | (*myADCH << 8);
 } /* adc_read */
-void usart_init(unsigned int ubbr)
-{       /* set baud rate */
-        *myUBRR0H = (unsigned char)ubbr >> 8;
-        *myUBRR0L = (unsigned char)ubbr;
-        /* enable RXEN0 and TXEN0 */
+
+void usart_init(unsigned int usart_baud_rate)
+{       /* set baud rate hi byte */
+        *myUBRR0H = (unsigned char)usart_baud_rate >> 8;
+        /* set baud rate lo byte */
+        *myUBRR0L = (unsigned char)usart_baud_rate;
+        /* enable reciever and transmitter */
         *myUCSR0B = 0b00011000;
         /* set frame format: 8 data bits, 2 stop bits */
         *myUCSR0C = 0b00001110;
 } /* usart_init */
+
+unsigned char usart_rx(void)
+{       /* wait until there are unread data in rx buffer */
+        while(!(*myUCSR0A & 0b10000000));
+        /* return data in rx buffer */
+        return *myUDR0;
+} /* usart_rx */
+
+void usart_tx(unsigned char usart_tx_data)
+{       /* wait until there are no data in tx buffer */
+        while(!(*myUCSR0A & 0b00100000));
+        /* set tx buffer to usart_tx_data */
+        *myUDR0 = usart_tx_data;
+} /* usart_tx */
+
 void setup(void)
-{       usart_init((fosc / (16 * baud)) - 1);
+{       usart_init((16000000 / (16 * 9600)) - 1);
         adc_init();
 } /* setup */
+
 void loop(void)
 {       (void)adc_read(0x00);
-        
-        unsigned char c;
-        while(u0_kbhit() == 0){};
-        c = u0_getchar();
-        u0_putChar(c);
+        unsigned char c = usart_rx();
+        usart_tx(c);
 } /* loop */
-
-//Reads RDA status bit from USART0 & return true if set
-unsigned char u0_kbhit(){
-    if(*myUSCR0A & RDA) return 1;
-    else return 0;
-}
-
-//read input from USART0 input buffer
-unsigned char u0_getchar(){
-    return *myUDRO;
-}
-
-//Waits for TBE to be set in UCSROA and write character to transmit buffer
-void u0_putChar(unsigned char u0pdata){
-    while(!(*myUSCR0A & TBE));
-    *myUDRO = u0pdata;
-}
-
-// void putstr(unsigned char* str)
-// {       while(*str)
-//         {       putchar(*str++);
-//         }
-//         putchar('\n');
-// }
